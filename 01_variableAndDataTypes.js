2d40316e42ecaf51b7bc265868def88ca7547452
Anshal543 <sherhasan543@gmail.com>
2024-07-13 20:20:27 +0500

    variables and datatypes

---
+++ 01_variableAndDataTypes.js
@@ -0,0 +1,54 @@
+//  Task 1: declare variable using var
+
+var num = 10;
+console.log(num);
+
+// Task 2: declare variable using let
+
+let username = "John";
+console.log(username);
+
+// Task 3: declare variable using const
+
+const PI = 3.14;   
+console.log(PI);
+
+// Task 4: data types in javascript
+
+let username1 = "John";
+let age = 25;
+let bool = false;
+let obj = {name: "John", age: 25};
+let arr = [1, 2, 3, 4, 5];
+console.log(username1,typeof username1);
+console.log(age,typeof age);
+console.log(bool,typeof bool);
+console.log(obj,typeof obj);
+console.log(arr,typeof arr);
+
+// Task 5: reassigning variables
+
+let num1 = 10;
+console.log(num1);
+num1 = "John";
+console.log(num1);
+
+// Task 6: const variable reassigning
+
+const num2 = 10;
+console.log(num2);
+// num2 = 20;
+// console.log(num2);
+
+// ressignment demo
+let num3 = 10;
+console.log(num3);
+num3 = 20;
+console.log(num3);
+
+const num4 = 10;
+console.log(num4);
+// num4 = 20; // error because we can't reassign const variable
+// console.log(num4);
+
+
