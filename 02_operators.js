8f80bef0faf9d8c474200f58d82f43310a7ed801
Anshal543 <sherhasan543@gmail.com>
2024-07-14 21:19:21 +0500

    operator

---
+++ 02_operators.js
@@ -0,0 +1,112 @@
+// arithematic operators
+
+// Task 1
+
+let a = 10;
+let b = 20;
+console.log(a + b);
+
+// Task 2
+
+console.log(a-b);
+
+// Task 3
+
+console.log(a*b);
+
+// Task 4
+
+console.log(a/b);
+
+// Task 5
+// find remainder
+
+console.log(a%b);
+
+// asignment operator
+
+// Task 6
+
+let x = 10;
+let y = 20;
+x += y;
+console.log(x,y);
+
+// Task 7
+
+x -= y;
+console.log(x,y);
+
+// comparison operator
+
+// Task 8
+
+console.log(a > b);
+
+// Task 9
+
+console.log(a < b);
+
+// Task 10
+
+console.log(a == b);
+
+// Task 11
+
+console.log(a != b);
+
+// Task 12
+
+console.log(a >= b);
+
+// Task 13
+
+console.log(a <= b);
+
+console.log(a===b);
+
+// == vs ===
+
+// Task 14
+
+let c = 10;
+let d = '10';
+console.log(c == d);
+
+// Task 15
+
+console.log(c === d);
+
+
+// logical operator
+
+// Task 16
+
+let e = true;
+let f = false;
+console.log(e && f);
+
+// Task 17
+
+console.log(e || f);
+
+// Task 18
+
+console.log(!f);
+
+// ternaary operator
+
+// Task 19
+
+let g = 10;
+let h = 20;
+let result = g > h ? 'greater' : 'smaller';
+console.log(result);
+
+// check if the number is positive or negative
+
+// Task 20
+
+let i = 10;
+let result1 = i > 0 ? 'positive' : 'negative';
+console.log(result1);
